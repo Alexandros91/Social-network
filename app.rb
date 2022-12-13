2eb07dfd4bffc788f451191088b3cc5ca67d6cac
Rippling Admin <ripplingadmin@alexanders-mbp.home>
2022-12-13 12:14:38 +0000

    app.rb file prints the #all and #find method function

--- app.rb
+++ app.rb
@@ -1,3 +1,23 @@
 require_relative 'lib/database_connection'
+require_relative 'lib/account_repository'
+require_relative 'lib/post_repository'
+
 
 DatabaseConnection.connect('social_network')
+
+account_repository = AccountRepository.new
+post_repository = PostRepository.new
+
+account_repository.all.each do |account|
+  p account
+end
+
+post_repository.all.each do |post|
+  p post
+end
+
+account = account_repository.find(1)
+puts " #{account.id} - #{account.email} - #{account.username}"
+
+post = post_repository.find(1)
+puts " #{post.id} - #{post.title} - #{post.content} - #{post.views} - #{post.account_id}"
